1d29ee83a790ebd1f62ab43dce26c741c523de70
abyblackmouth <abraham.covarrubias@live.com>
2023-05-01 16:42:45 -0600

    started PortManager front

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+ return (
+    <>
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to Administrador de cobranza
+        </h1>
+      </main>
+    </>  
+  )
+}
