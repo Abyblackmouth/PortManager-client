e81fe0dd21b370be110713ac3cb707d405525440
abyblackmouth <abraham.covarrubias@live.com>
2023-05-01 17:12:46 -0600

    add Nav component

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return(
+    <nav className="w-full h-9 bg-blue-400 flex items-center">
+       <h1 className="pl-5 flex">
+        Bienvenidos a Santega
+       </h1>
+    </nav>
+  )
+
+}
